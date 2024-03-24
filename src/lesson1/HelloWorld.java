f82540ee6939150cd8f2b451e7fa3bee326c23d1
AlonaSobachko <alonka.sobacho@gmail.com>
2024-03-24 21:43:52 +0200

    clear all

---
+++ src/lesson1/HelloWorld.java
@@ -0,0 +1,21 @@
+package lesson1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        System.out.println("Привіт!");
+        System.out.println();
+        System.out.println("Моє ім'я занадто значиме, щоб я говорив його вголос.");
+        System.out.println("А ти хто?");
+
+        System.out.println("кон");
+        System.out.println("соль");
+
+        System.out.println("бал");
+        System.out.println("кон");
+
+        System.out.println("бал");
+        System.out.println("бес");
+    }
+}
