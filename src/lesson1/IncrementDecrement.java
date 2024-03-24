c054de83d8a55725404973356d3048619dd7e1a0
AlonaSobachko <alonka.sobacho@gmail.com>
2024-03-24 23:06:23 +0200

    Change

---
+++ src/lesson1/IncrementDecrement.java
@@ -0,0 +1,30 @@
+package lesson1;
+
+public class IncrementDecrement {
+    public static void main(String[] args) {
+        // ++
+        // a++ equals a = a + 1
+        int a = 13;
+        a++;
+        System.out.println(a);
+        int c = 13;
+        c = c*3;
+        System.out.println("c = " + c);
+
+        // a-- equals a = a - 1
+        c--;
+        System.out.println(c);
+
+        // a+=7 a = a + 7
+        int b = 2;
+        b+=4;
+        System.out.println(b);
+        b*=2;
+        System.out.println(b);
+        b-=2;
+        System.out.println(b);
+        b/=5;
+        System.out.println(b);
+
+    }
+}
