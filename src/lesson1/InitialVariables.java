343e7aba0bcf41523dfb30d432c316c03f6769c6
AlonaSobachko <alonka.sobacho@gmail.com>
2024-03-24 22:14:59 +0200

    Change

---
+++ src/lesson1/InitialVariables.java
@@ -0,0 +1,16 @@
+package lesson1;
+
+public class InitialVariables {
+    public static void main(String[] args) {
+        System.out.println("14 / 2");
+        int firstNumber;
+        firstNumber = 14;
+        int secondNumber = 2;
+        int x, y;
+        int result = firstNumber / secondNumber;
+        System.out.println(result);
+        result = 44;
+        System.out.println(result);
+
+    }
+}
