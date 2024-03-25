b183ed20cf1a4c8eeac3e584be46999b78826f44
AlonaSobachko <alonka.sobacho@gmail.com>
2024-03-26 00:57:28 +0200

    Change

---
+++ src/lesson2/Lesson2Task2.java
@@ -0,0 +1,18 @@
+package lesson2;
+
+public class Lesson2Task2 {
+    // x = 5;
+    // y = 4 + x++;
+    // y+= --y - x;
+    //  z = y++;
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 4 + x++;
+        y+= --y - x;
+        int z = y++;
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+
+    }
+}
