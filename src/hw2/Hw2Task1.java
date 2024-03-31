f92d9a12227a6c521e61d6ff73cf3adc6c407b23
AlonaSobachko <alonka.sobacho@gmail.com>
2024-03-31 22:42:35 +0300

    Home wokr 2

---
+++ src/hw2/Hw2Task1.java
@@ -0,0 +1,22 @@
+package hw2;
+
+public class Hw2Task1 {
+    //Створити змінну string1 = "This line that i want to cut, cause it is too long".
+    //Створити рядок string2 у якому має бути розміщено значення рядка string1,
+    //Обрізане до "This line that i want to cut, cause".
+    //Створити рядок string3 на основі string2 який міститиме значення
+    //"This line that don't want to cut, cause it is perfect".
+    //Вивести на консоль кожне повідомлення та його довжину.
+    public static void main(String[] args) {
+        String string1 = "This line that I want to cut, cause it is too long";
+        String string2 = string1.substring(0, 35);
+        String string3 = string2.replace("I", "don`t").concat(" it is perfect");
+        System.out.println(string1);
+        System.out.println("Довжина string1 дорівнює " + string1.length());
+        System.out.println(string2);
+        System.out.println("Довжина string2 дорівнює " + string2.length());
+        System.out.println(string3);
+        System.out.println("Довжина string3 дорівнює " + string3.length());
+
+    }
+}
---
+++ src/hw2/Hw2Task2.java
@@ -0,0 +1,21 @@
+package hw2;
+
+public class Hw2Task2 {
+    //Створити рядок string = "Testing, is my favourite job".
+    //Вивести на екран окремо кожне слово та довжину цього слова у вигляді:
+    //Слово1 = (значення слова), Довжина цього слова = (значення довжини слів).
+    //Слово2 = (значення слова), Довжина цього слова = (значення довжини слів).
+    //і т.д.
+    //Вивести на консоль true, якщо перше слово довше інших, в іншому випадку вивести false.
+    public static void main(String[] args) {
+        String string = "Testing, is my favourite job";
+        String[] strings = string.split(" ");
+        System.out.println("Слово 1 = " + strings[0].replace(",", " ")
+                .concat("Довжина цього слова = ") + strings[0].length());
+        System.out.println("Слово 2 = " + strings[1].concat(" Довжина цього слова = ")
+                + strings[1].length());
+        System.out.println("Слово 3 = " + strings[2] + " Довжина цього слова = " + strings[2].length());
+        System.out.println("Слово 4 = " + strings[3] + " Довжина цього слова = " + strings[3].length());
+        System.out.println("Слово 5 = " + strings[4] + " Довжина цього слова = " + strings[4].length());
+    }
+}
