4ecad81f28e63e22530625b99b96ae87a54a7fd4
AlonaSobachko <alonka.sobacho@gmail.com>
2024-04-02 18:14:03 +0300

    Home wokr 2 completed

--- src/hw2/Hw2Task2.java
+++ src/hw2/Hw2Task2.java
@@ -1,13 +1,14 @@
 package hw2;
 
 public class Hw2Task2 {
-    //Створити рядок string = "Testing, is my favourite job".
-    //Вивести на екран окремо кожне слово та довжину цього слова у вигляді:
-    //Слово1 = (значення слова), Довжина цього слова = (значення довжини слів).
-    //Слово2 = (значення слова), Довжина цього слова = (значення довжини слів).
-    //і т.д.
-    //Вивести на консоль true, якщо перше слово довше інших, в іншому випадку вивести false.
     public static void main(String[] args) {
+        //Створити рядок string = "Testing, is my favourite job".
+        //Вивести на екран окремо кожне слово та довжину цього слова у вигляді:
+        //Слово1 = (значення слова), Довжина цього слова = (значення довжини слів).
+        //Слово2 = (значення слова), Довжина цього слова = (значення довжини слів).
+        //і т.д.
+        //Вивести на консоль true, якщо перше слово довше інших, в іншому випадку вивести false.
+
         String string = "Testing, is my favourite job";
         String[] strings = string.split(" ");
         System.out.println("Слово 1 = " + strings[0].replace(",", " ")
@@ -20,15 +21,5 @@ public class Hw2Task2 {
         System.out.println(strings[0].length() > strings[1].length() && strings[0].length() >
                 strings[2].length() && strings[0].length() > strings[3].length() && strings[0].length() >
                 strings[4].length());
-        if (strings[0].length() > strings[1].length()) {
-            System.out.println(true);
-        } else if (strings[0].length() > strings[2].length()) {
-            System.out.println(true);
-        } else if (strings[0].length() > strings[3].length()) {
-            System.out.println(true);
-        } else if (strings[0].length() > strings[4].length()) {
-            System.out.println(true);
-        }
-        else System.out.println(false);
     }
 }
