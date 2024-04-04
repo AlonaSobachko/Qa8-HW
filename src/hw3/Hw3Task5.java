fa1326b112c75c73b03649597561beacb658f855
AlonaSobachko <alonka.sobacho@gmail.com>
2024-04-04 20:33:18 +0300

    hw

--- src/hw3/Hw3Task5.java
+++ src/hw3/Hw3Task5.java
@@ -7,9 +7,6 @@ public class Hw3Task5 {
     // і символ – + або % або / або *. На екран виводиться результат дії над двома введеними числами.
     // Якщо користувач ввів щось окрім даних символів, необхідно вивести 0.
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Введіть два числа та один з наведених символів: '-', '+', '%', '/', '*'");
-        int number1, number2  = sc.nextInt();
 
     }
 }
--- src/hw3/Hw3Task6.java
+++ src/hw3/Hw3Task6.java
@@ -10,9 +10,67 @@ public class Hw3Task6 {
     // в консоль, про те, що такої програми не існує. Якщо зазначеної користувачем ОС немає, виводиться
     // повідомлення в консоль, що такої ОС немає.
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Яка програма Вас цікавить??");
-        String prog = sc.nextLine();
-        switch (prog.toLowerCase()){}
+        String program = scanner.nextLine();
+        switch (program){
+            case ("IntelliJ IDEA"):
+                System.out.println("Яку ОС Ви використовуєте??");
+                String operatingSystem = scanner.nextLine();
+                switch (operatingSystem){
+                    case ("Windows"):
+                        System.out.println("https://www.jetbrains.com/idea/download/?section=windows");
+                        break;
+                    case ("MacOS"):
+                        System.out.println("https://www.jetbrains.com/idea/download/?section=mac");
+                        break;
+                    case ("Linux"):
+                        System.out.println("https://www.jetbrains.com/idea/download/?section=linux");
+                        break;
+                    default:
+                        System.out.println("Not found OS!!!");
+                        break;
+                }
+                break;
+            case ("Git"):
+                System.out.println("Яку ОС Ви використовуєте??");
+                String operatingSystem1 = scanner.nextLine();
+                switch (operatingSystem1){
+                    case ("Windows"):
+                        System.out.println("https://git-scm.com/download/win");
+                        break;
+                    case ("MacOS"):
+                        System.out.println("https://git-scm.com/download/mac");
+                        break;
+                    case ("Linux"):
+                        System.out.println("https://git-scm.com/download/linux");
+                        break;
+                    default:
+                        System.out.println("Not found OS!!!");
+                        break;
+                }
+                break;
+            case ("Java"):
+                System.out.println("Яку ОС Ви використовуєте??");
+                String operatingSystem2 = scanner.nextLine();
+                switch (operatingSystem2){
+                    case ("Windows"):
+                        System.out.println("https://javadl.oracle.com/webapps/download/AutoDL?BundleId=249551_4d245f941845490c91360409ecffb3b4");
+                        break;
+                    case ("MacOS"):
+                        System.out.println("https://javadl.oracle.com/webapps/download/AutoDL?BundleId=249545_4d245f941845490c91360409ecffb3b4");
+                        break;
+                    case ("Linux"):
+                        System.out.println("https://javadl.oracle.com/webapps/download/AutoDL?BundleId=249540_4d245f941845490c91360409ecffb3b4");
+                        break;
+                    default:
+                        System.out.println("Not found OS!!!");
+                        break;
+                }
+                break;
+            default:
+                System.out.println("Not found program!!!");
+                break;
+        }
+        }
     }
-}
