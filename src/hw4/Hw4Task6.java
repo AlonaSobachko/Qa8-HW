bd2a2106204ff9a80c8e4d72a47aca96297651a7
AlonaSobachko <alonka.sobacho@gmail.com>
2024-04-11 15:06:30 +0300

    hw completed

--- src/hw4/Hw4Task6.java
+++ src/hw4/Hw4Task6.java
@@ -10,20 +10,22 @@ public class Hw4Task6 {
     public static void main(String[] args) {
         int[] array = new int[45];
         Random random = new Random();
-        for (int i = 0; i < array.length; i++){
+        for (int i = 0; i < array.length; i++) {
             array[i] = random.nextInt(-50, 50);
-            int min = array[0];
-            int max = array[0];
-            for (int num : array){
-                if (num < min){
-                    min = num;
-                }
-                if (num > max){
-                    max = num;
-                }
-            }
-            System.out.println("Мінімальний елемент масиву: " + min);
-            System.out.println("Максимальни елемент масиву: " + max);
         }
+        System.out.println(Arrays.toString(array));
+        int min = array[0];
+        int max = array[0];
+        for (int num : array) {
+            if (num < min) {
+                min = num;
+            }
+            if (num > max) {
+                max = num;
+            }
+        }
+        System.out.println("Мінімальний елемент масиву: " + min);
+        System.out.println("Максимальни елемент масиву: " + max);
+
     }
 }
